145cc483fa3ca28f12b59b7c7cd4024261ef3b18
_DQ_ <_DQ_@NOTEBOOK>
2017-11-11 19:33:48 +0300

    Дополнен вывод ToString(); добавлена генерация случайных строк в тесты создания групп

--- AddressbookWebTest/AddressbookWebTest/tests/GroupCreationTests.cs
+++ AddressbookWebTest/AddressbookWebTest/tests/GroupCreationTests.cs
@@ -10,13 +10,27 @@ namespace WebAddressbookTests
     [TestFixture]
     public class GroupCreationTests : AuthTestBase
     {
-        [Test]
-        public void GroupCreationTest()
+        public static IEnumerable<GroupData> RandomGroupDataProvider()
         {
-            GroupData group = new GroupData("BestGroup");
+            List<GroupData> groups = new List<GroupData>();
+            for (int i =0; i<5; i++)
+            {
+                groups.Add(new GroupData(GenerateRandomString(30))
+                {
+                    Header = GenerateRandomString(100),
+                    Footer = GenerateRandomString(100)
+                });
+            }
+            return groups;
+        }
+
+        [Test, TestCaseSource("RandomGroupDataProvider")]
+        public void GroupCreationTest(GroupData group)
+        {
+            /*GroupData group = new GroupData("BestGroup");
             group.Header = "header";
             group.Footer = "footer";
-
+            */
             app.Navigator.GoToGroupsPage();
             List<GroupData> oldGroups = app.Groups.GetGroupList();
             app.Groups.Create(group);
@@ -29,25 +43,6 @@ namespace WebAddressbookTests
             //app.Auth.Logout();
         }
 
-        [Test]
-        public void GroupEmptyNameCreationTest()
-        {
-            GroupData group = new GroupData("");
-            group.Header = "";
-            group.Footer = "";
-
-            app.Navigator.GoToGroupsPage();
-            List<GroupData> oldGroups = app.Groups.GetGroupList();
-            app.Groups.Create(group);
-            Assert.AreEqual(oldGroups.Count + 1, app.Groups.GetGroupCount());
-            List<GroupData> newGroups = app.Groups.GetGroupList();
-            oldGroups.Add(group);
-            oldGroups.Sort();
-            newGroups.Sort();
-            Assert.AreEqual(oldGroups, newGroups);
-
-            //app.Auth.Logout();
-        }   
         [Test]
         public void BadNameGroupCreationTest()
         {
--- AddressbookWebTest/AddressbookWebTest/tests/TestBase.cs
+++ AddressbookWebTest/AddressbookWebTest/tests/TestBase.cs
@@ -22,6 +22,19 @@ namespace WebAddressbookTests
             app.Navigator.GoToHomePage();
         }
 
-        
+        public static Random rnd = new Random();
+
+        public static string GenerateRandomString(int max)
+        {
+            int l = Convert.ToInt32(rnd.NextDouble() * max);
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < l; i++)
+            {
+                builder.Append(Convert.ToChar(32 + Convert.ToInt32(rnd.NextDouble() * 223)));
+            }
+            return builder.ToString();
+        }
+
+
     }
 }
