b5d577904d1c730b43d5f8b209006ea96355ac84
_DQ_ <_DQ_@NOTEBOOK>
2017-11-20 22:01:32 +0300

    Создан RemovingContactFromGroupTest

--- AddressbookWebTest/AddressbookWebTest/AppManager/ContactHelper.cs
+++ AddressbookWebTest/AddressbookWebTest/AppManager/ContactHelper.cs
@@ -37,6 +37,26 @@ namespace WebAddressbookTests
             };
         }
 
+        public void RemoveContactFromGroup(ContactData contact, GroupData group)
+        {
+            manager.Navigator.GoToHomePage();
+            SelectGroupToRemoveFrom(group.Name);
+            SelectContact(contact.ID);
+            CommitRemovingContactFromGroup();
+            new WebDriverWait(driver, TimeSpan.FromSeconds(10))
+                .Until(d => driver.FindElements(By.CssSelector("div.msgbox")).Count > 0);
+        }
+
+        public void SelectGroupToRemoveFrom(string name)
+        {
+            new SelectElement(driver.FindElement(By.Name("group"))).SelectByText(name);
+        }
+
+        public void CommitRemovingContactFromGroup()
+        {
+            driver.FindElement(By.Name("remove")).Click();
+        }
+
         public void AddContactToGroup(ContactData contact, GroupData group)
         {
             manager.Navigator.GoToHomePage();
---
+++ AddressbookWebTest/AddressbookWebTest/tests/RemovingContactFromGroupTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WebAddressbookTests
+{
+    class RemovingContactFromGroupTests : AuthTestBase
+    {
+        [Test]
+        public void RemovingContactFromGroupTest()
+        {
+            GroupData group = GroupData.GetAllFromDB()[0];
+            List<ContactData> oldList = group.GetContactInGroup();
+            ContactData contact; // = ContactData.GetAllFromDB().Except(oldList).First();
+            contact = oldList.First();
+            /*try
+            {
+                contact = ContactData.GetAllFromDB().Except(oldList).First();
+            }
+            catch (System.InvalidOperationException)
+            {
+                System.Console.Out.WriteLine("все контакты уже состоят в выбранной группе");
+                throw;
+            }*/
+
+            app.Contacts.RemoveContactFromGroup(contact, group);
+
+            List<ContactData> newList = group.GetContactInGroup();
+            oldList.Remove(contact);
+            oldList.Sort();
+            newList.Sort();
+
+            Assert.AreEqual(oldList, newList);
+        }
+    }
+}
